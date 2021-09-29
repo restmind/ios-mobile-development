ddceca8a95b539dee1bec6e4ee745dd99f7a6c6a
Shuplat Orest <shuplatorst@gmail.com>
2021-09-29 22:44:47 +0300

    Adding finished first lab

---
+++ mobile-dev/mobile_devApp.swift
@@ -0,0 +1,17 @@
+//
+//  mobile_devApp.swift
+//  mobile-dev
+//
+//  Created by Orest on 29.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct mobile_devApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
